da204a0c78348788f645418bd982f741af916585
Rob Moore <rmoore@gradle.com>
2022-05-11 15:35:40 +0800

    Configuration cache fixes

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -1,17 +1,17 @@
-val outputFile = project.layout.buildDirectory.dir("some-subdir").map { it.file("shared-file.txt") }
+val sharedFile = project.layout.buildDirectory.file("some-subdir/shared-file.txt")
+
 val makeFile = tasks.register("makeFile") {
-    outputs.file(outputFile)
+    outputs.file(sharedFile)
     doFirst {
-        outputFile.get().asFile
-            .writeText("This file is shared across Gradle subprojects.")
+        sharedFile.get().asFile.writeText("This file is shared across Gradle subprojects.")
     }
 }
 
-val sharedConfiguration: Configuration by configurations.creating {
+val sharedConfiguration by configurations.creating {
     isCanBeConsumed = true
     isCanBeResolved = false
 }
 
 artifacts {
-    add(sharedConfiguration.name, makeFile)
+    add(sharedConfiguration.name, sharedFile)
 }
